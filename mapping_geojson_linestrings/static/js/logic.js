012ce7f3ef813721553856f60f11ad0a64ba6bc8
Danny Chu <dannyvchu@gmail.com>
2021-07-15 07:13:00 -0500

    test

--- mapping_geojson_linestrings/static/js/logic.js
+++ mapping_geojson_linestrings/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log('working');
 
 // Street style tile layer
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -25,7 +25,7 @@ let baseMaps = {
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
     layers: [streets]
 });
@@ -34,54 +34,23 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 // Add GeoJSON data.
 
-// let sanFranAirport = 
-// {"type":"FeatureCollection","features":[{
-//     'type':'Feature',
-//     'properties':{
-//         'id':'3469',
-//         'name':'San Francisco International Airport',
-//         'city':'San Francisco',
-//         'country':'United States',
-//         'faa':'SFO',
-//         'icao':'KSFO',
-//         'alt':'13',
-//         'tz-offset':'-8',
-//         'dst':'A',
-//         'tz':'America/Los_Angeles'},
-//         "geometry":{
-//             'type':'Point',
-//             'coordinates':[-122.375,37.61899948120117]}}
-    
-// ]};
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = 'https://raw.githubusercontent.com/dannyvchu/mapping_earthquakes/mapping_geojson_linestrings/torontoRoutes.json';
+
+// Create a style for the lines.
+let myStyle = {
+    color: 'yellow',
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-
-// L.geoJSON(sanFranAirport, {
-//     // We turn each feature into a marker on the map.
-//     pointToLayer: (feature, latlng) => {
-//         console.log(feature);
-//         return L.marker(latlng)
-//         .bindPopup('<h2>' + feature.properties.city + '</h2>');
-//     }
-// }).addTo(map);
-
-// L.geoJson(sanFranAirport, {
-//     onEachFeature: (feature,layer) => {
-//         console.log(layer);
-//         layer.bindPopup();
-//     }
-// }).addTo(map);
-
-//Accessing the airport GeoJSON URL
-let airportData = 'https://raw.githubusercontent.com/dannyvchu/mapping_earthquakes/mapping_geojson_points/majorAirports.json';
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(data => {
+d3.json(torontoData).then(data => {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
+        style: myStyle,
         onEachFeature: (feature,layer) => {
-            console.log(feature);
-            layer.bindPopup('<h2>' + feature.properties.city + '</h2>');
+            layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3> <hr><h3> Destination: ' + feature.properties.dst + '</h3>');
         }
     }).addTo(map);
 });
